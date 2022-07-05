2b9188c4baef88855f18af9b940ebefc085421d0
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2022-07-05 22:43:05 +0300

    Added event for rated button

--- src/helpers/createEventListeners.ts
+++ src/helpers/createEventListeners.ts
@@ -1,4 +1,4 @@
-import { PopularPage, UpcomingPage } from "./handlers";
+import { PopularPage, UpcomingPage,RatedPage } from "./handlers";
 
 export const createEventListeners = async (): Promise<void> => {
 	const popularBtn = document.getElementById("popular");
@@ -6,4 +6,7 @@ export const createEventListeners = async (): Promise<void> => {
 
 	const upcomingBtn = document.getElementById("upcoming");
 	upcomingBtn && (upcomingBtn.onclick = () => UpcomingPage());
+
+	const ratedBtn = document.getElementById("top_rated");
+	ratedBtn && (ratedBtn.onclick = () => RatedPage());
 }
