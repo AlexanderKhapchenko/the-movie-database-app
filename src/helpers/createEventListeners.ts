d5c5c7321493f805801863e0ceeadf24286a35d0
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2022-07-05 22:42:35 +0300

    Added event for upcoming button

--- src/helpers/createEventListeners.ts
+++ src/helpers/createEventListeners.ts
@@ -1,6 +1,9 @@
-import { PopularPage } from "./handlers";
+import { PopularPage, UpcomingPage } from "./handlers";
 
 export const createEventListeners = async (): Promise<void> => {
 	const popularBtn = document.getElementById("popular");
 	popularBtn && (popularBtn.onclick = () => PopularPage());
+
+	const upcomingBtn = document.getElementById("upcoming");
+	upcomingBtn && (upcomingBtn.onclick = () => UpcomingPage());
 }
